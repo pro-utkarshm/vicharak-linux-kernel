6c4c7ca82f5511af5bddc747b7857c9fe89ca57c
UtsavBalar1231 <utsavbalar1231@gmail.com>
2023-10-28 14:31:36 +0530

    gpio: pca953x: Add support to set initial status of expander IC
    Change-Id: Ia58fa394f22cc5860c165cec455da3fb3b8a095e
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/gpio/gpio-pca953x.c
+++ drivers/gpio/gpio-pca953x.c
@@ -946,6 +946,33 @@ out:
 	return ret;
 }
 
+static int expander_spi_mode(struct pca953x_chip *chip)
+{
+	int ret = 0;
+
+	if (!chip)
+		return -ENODEV;
+
+	// set pin direction to output
+	ret = pca953x_gpio_direction_output(&chip->gpio_chip, 1, 1);
+	if (ret)
+		return ret;
+	ret = pca953x_gpio_direction_output(&chip->gpio_chip, 2, 1);
+	if (ret)
+		return ret;
+
+	// Print the number of pins
+	dev_info(&chip->client->dev, "Number of pins: %d\n",
+		 chip->gpio_chip.ngpio);
+
+	pca953x_gpio_set_value(&chip->gpio_chip, 1, 1);
+	pca953x_gpio_set_value(&chip->gpio_chip, 2, 1);
+
+	dev_info(&chip->client->dev, "spi mode set properly\n");
+
+	return 0;
+}
+
 static int pca953x_probe(struct i2c_client *client,
 			 const struct i2c_device_id *i2c_id)
 {
@@ -1074,6 +1101,10 @@ static int pca953x_probe(struct i2c_client *client,
 	ret = devm_gpiochip_add_data(&client->dev, &chip->gpio_chip, chip);
 	if (ret)
 		goto err_exit;
+
+	ret = expander_spi_mode(chip);
+	if (ret)
+		dev_warn(&client->dev, "spi mode set failed, %d\n", ret);
 
 	if (pdata && pdata->setup) {
 		ret = pdata->setup(client, chip->gpio_chip.base,
