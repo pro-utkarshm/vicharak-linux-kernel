86bf2ae6eeed9d33ccd60b6123c21940cf3422dc
vatsalkevadiya08141 <vhkevadiya15@gmail.com>
2025-01-31 17:49:14 +0530

    linux: peripheral: update accordingly to the periplex bus.
    - `module_periplex_driver()` is use for driver registration.
    - `periplex_set_drvdata()` function use for set data for that
       driver.
    - `periplex_get_drvdata()` function use for get data for that
       driver.

--- include/linux/peripheral.h
+++ include/linux/peripheral.h
@@ -1,8 +1,8 @@
-#define PERIPHERAL_H
-#define PERIPHERAL_H
+#ifndef _PERIPHERAL_H
+#define _PERIPHERAL_H
 
-#include <linux/platform_device.h>
-#include <linux/export.h>
+#include <linux/device.h>
+#include <linux/module.h>
 
 /* Common Structure for congigration frame(use in write case) */
 struct kernel_config
@@ -20,7 +20,7 @@ struct kernel_data
     int minor;
 };
 
-/* common structure for read data (use in read case) */
+/* common structure used in read case */
 struct read_buffer
 {
     char *message;
@@ -28,25 +28,58 @@ struct read_buffer
     int minor;
 };
 
-/* periplex device structure */
+/*set configuration for a specific peripheral(use in write case) */
+void set_periplex_configuration(int peri_id, uint8_t config_id,
+                                int configuration);
+
+/*set data for a specific peripheral (use in write case) */
+void set_periplex_data(int peri_id, int length, char *message);
+
+/* Structure representing periplex device */
 struct periplex_device
 {
-    struct platform_device *pdev;
     void *data;
-    int (*get_periplex_data) (struct periplex_device *dev, 
-    char *message, const int len);
+    int periplex_id;
+    const char *name;
+    struct device dev;
+    int (*get_periplex_data)(struct periplex_device *pdev,
+                             char *message, const int len);
 };
 
-/* use for set configurations */
-void set_periplex_configuration(int peri_id, uint8_t config_id, 
-        int configuration);
+/* Structure representing periplex driver */
+struct periplex_driver
+{
+    struct device_driver driver;
+    int id;
+    int (*probe)(struct periplex_device *pdev);
+    int (*remove)(struct periplex_device *pdev);
+};
 
-/* use for set data */
-void set_periplex_data(int peri_id,int length, char*message);
+/* periplex bus type */
+extern struct bus_type periplex_bus_type;
 
-/* use for register device to periplex */
-int periplex_device_register(struct platform_device *pdev, 
-        int periplex_id);
+/* Function prototypes for bus operations */
+extern int periplex_link_device(struct periplex_device *pdev);
+extern void periplex_unlink_device(struct periplex_device *pdev);
+extern int periplex_register_driver(struct periplex_driver *drv);
+extern void periplex_unregister_driver(struct periplex_driver *drv);
 
-/* use for unregister device from periplex */
-void periplex_device_unregister(struct platform_device *pdev);
+/* Helper macros to convert between types */
+#define to_periplex_device(d) container_of(d, struct periplex_device, dev)
+#define to_periplex_driver(d) container_of(d, struct periplex_driver, driver)
+
+/* Macro to simplify driver registration */
+#define module_periplex_driver(__drv) \
+    module_driver(__drv, periplex_register_driver, periplex_unregister_driver)
+
+static inline void *periplex_get_drvdata(const struct periplex_device *pdev)
+{
+    return dev_get_drvdata(&pdev->dev);
+}
+
+static inline void periplex_set_drvdata(struct periplex_device *pdev, void *data)
+{
+    dev_set_drvdata(&pdev->dev, data);
+}
+
+#endif /* _PERIPHERAL_H */
